05f08a02fb5ba1d669612593cc7aa2db13a11be6
Mohsin <mohsinshah3171@gmail.com>
2023-12-26 13:14:27 +0500

    NEW: Font

--- datacrunch-clone/src/layouts/Navbar/Navbar.jsx
+++ datacrunch-clone/src/layouts/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import { navbar, navigator, auth } from './Navbar.module.css';
 export default function Navbar() {
   return (
     <div className={navbar}>
+      <img src="https://datacrunch.io/_next/static/media/Logo.99886008.svg" alt="logo" />
       <div className={navigator}>
         <span>GPU Instances</span>
         <span>Clusters</span>
